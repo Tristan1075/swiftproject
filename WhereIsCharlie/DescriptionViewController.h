a1c450a70c06e5d2e6217b68e18ae11432be4cb1
muriel durand <you@example.com>
2019-02-12 04:54:09 +0100

    DetailView

---
+++ WhereIsCharlie/DescriptionViewController.h
@@ -0,0 +1,17 @@
+//
+//  DescriptionViewController.h
+//  WhereIsCharlie
+//
+//  Created by muriel durand on 12/02/2019.
+//  Copyright © 2019 Tristan Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DescriptionViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WhereIsCharlie/DescriptionViewController.m
@@ -0,0 +1,32 @@
+//
+//  DescriptionViewController.m
+//  WhereIsCharlie
+//
+//  Created by muriel durand on 12/02/2019.
+//  Copyright © 2019 Tristan Luong. All rights reserved.
+//
+
+#import "DescriptionViewController.h"
+
+@interface DescriptionViewController ()
+
+@end
+
+@implementation DescriptionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
